ce20f9abc3b4bf3e7efb0268db6cf0a15ce32400
Ebuka Chris <ebukachris09@gmail.com>
2026-06-13 14:16:16 +0100

    merging bloglist frontend and backend together

--- bloglist-backend/app.js
+++ bloglist-backend/app.js
@@ -1,28 +1,29 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const app = express()
+const path = require('path')
 
+const app = express()
 
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
-
 
 const blogsRouter = require('./controllers/blog')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URI, { family: 4 })
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(express.json())
 app.use(cors())
+
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 app.use(middleware.userExtractor)
@@ -31,17 +32,19 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
-
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
 }
 
+// Serve React build
+app.use(express.static(path.join(__dirname, 'dist')))
 
-app.use(middleware.unknownEndpoint)
+// SPA fallback
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
 app.use(middleware.errorHandler)
 
 module.exports = app
-
-
-
